0bf1da7496c12136d14deadc267e0ef61186b427
Higor Chaves <higor.chaves@azi.com.br>
2019-06-04 21:39:03 -0400

    Melhoria - organização automática das boxes

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,17 @@
   <div id="app">
     <div class="grid">
       <div class="row">
-        <div v-for="api in apis" :key="api.id" class="col-md-4">
+        <div
+          v-for="api in apis"
+          :key="api.id"
+          class="cols-boxes"
+          :class="{
+            'col-12' : apis.length == 1,
+            'col-6' : apis.length == 2,
+            'col-4' : apis.length >= 3 & apis.length <= 6,
+            'col-3' : apis.length > 6
+           }"
+        >
           {{api.id}}
           <Box :nome="api.nome" :url="api.url" :timeToReload="api.timeToReload"/>
         </div>
@@ -34,6 +44,21 @@ export default {
           nome: "W3S",
           url: "https://www.w3schools.com",
           timeToReload: "10000"
+        },
+        {
+          nome: "G1",
+          url: "https://g1.globo.om/",
+          timeToReload: "10000"
+        },
+        {
+          nome: "Google",
+          url: "https://google.com/",
+          timeToReload: "10000"
+        },
+        {
+          nome: "W3S",
+          url: "https://www.w3schools.com",
+          timeToReload: "10000"
         }
       ]
     };
@@ -45,7 +70,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="less">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -54,7 +79,15 @@ export default {
   color: #2c3e50;
 }
 
+.cols-boxes {
+  margin-bottom: 10px;
+}
+
 .grid {
   margin: 10px;
+  .row {
+    margin-right: 0;
+    margin-left: 0;
+  }
 }
 </style>
